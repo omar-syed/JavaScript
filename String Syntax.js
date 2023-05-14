5ced3376ffb2afa98a2add2ae299934ab590e7af
omar-syed <omarrrrr118@gmail.com>
2023-05-14 20:50:04 +0300

    2 basics

---
+++ String Syntax.js
@@ -0,0 +1,13 @@
+/*
+ String Syntax And Character Escape Sequences
+ \Escape + Line Continue
+ \n
+*/
+//console.log('Omar Elsayed "Web Devolper"');
+console.log("Omar Elsayed \"Web Devolper\"");
+console.log('Omar Elsayed\\ \'Web Devolper\'');
+console.log('Omar Elsayed\
+ "Web Devolper"\
+ ');
+ console.log('Omar\n Elsayed\n \'Web Devolper\'');
+
--- Variables.js
+++ Variables.js
@@ -7,6 +7,20 @@ console.log(age);
 console.log(hello);
 hello.innerHTML="Option";
 
+var a = 1 ;
+var a = 2;
+console.log(a);
+
+
+let a = 1 ;
+let a = 2; //not allow
+console.log(a);
+
+
+const a = 1 ;
+const a = 2;  // not allow
+console.log(a);
+
 
 
 
