9d3c7d44ad48fcb4f58cda9b30adaacaba30f55e
omar-syed <omarrrrr118@gmail.com>
2023-05-14 20:58:11 +0300

    edit string file

--- String Syntax.js
+++ String Syntax.js
@@ -3,6 +3,9 @@
  \Escape + Line Continue
  \n
 */
+
+
+
 //console.log('Omar Elsayed "Web Devolper"');
 console.log("Omar Elsayed \"Web Devolper\"");
 console.log('Omar Elsayed\\ \'Web Devolper\'');
@@ -10,4 +13,12 @@ console.log('Omar Elsayed\
  "Web Devolper"\
  ');
  console.log('Omar\n Elsayed\n \'Web Devolper\'');
+
+
+ // Concatenation
+ let a = "We Love"
+ let b = "JavaScript"
+ //document.write(a+b);
+document.write(a+" "+b);
+console.log(a, b);
 
