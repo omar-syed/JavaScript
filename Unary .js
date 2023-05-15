e75d118074ca146d0d108f941da0c3c72a4e2951
omar-syed <omarrrrr118@gmail.com>
2023-05-15 11:02:42 +0300

    Type Coercion

---
+++ Unary .js
@@ -0,0 +1,51 @@
+/*
+  - + Unary Plus [Return Number If Its Not Number]
+  - - Unary Negation [Return Number If Its Not Number + Negates It]
+  Tests
+  - Normal Number
+  - String Number
+  - String Negative Number
+  - String Text
+  - Float
+  - Hexadecimal Numeral System => 0xFF
+  - null
+  - false
+  - true
+*/
+
+console.log(+100);
+console.log(+"100");
+console.log(+"-100");
+console.log(+"Osama");
+console.log(+"15.5");
+console.log(+0xff);
+console.log(+null);
+console.log(+false);
+console.log(+true);
+
+console.log(-100);
+console.log(-"100");
+console.log(-"-100");
+console.log(-"Osama");
+console.log(-"15.5");
+console.log(-0xff);
+console.log(-null);
+console.log(-false);
+console.log(-true);
+
+console.log(Number("100"));
+
+
+/*
+  Type Coercion (Type Casting)
+  - +
+  - -
+  - "" - 2
+  - false - true
+*/
+
+let a = "100";
+let b = 20;
+let c = true;
+
+console.log(+a + b + c);
